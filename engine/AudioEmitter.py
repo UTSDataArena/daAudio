9f8834d1ec254a6c318c390cf0128d384a9f3942
luke.cole <luke.cole@uts.edu.au>
2017-08-01 10:35:47 +1000

    Add settters.

--- engine/AudioEmitter.py
+++ engine/AudioEmitter.py
@@ -8,3 +8,12 @@ class AudioEmitter(object):
         
         self.name = name
         self.source = SoundSource()
+
+    def set_position(self, position):
+        self.source.position = position
+
+    def set_looping(self, looping):
+        self.source.looping = looping
+
+    def queue(self, audio):
+        self.source.queue(audio)
